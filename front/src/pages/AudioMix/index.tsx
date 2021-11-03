93ce3d763fbd55fb3bcb9e1bb2fd5f8eb719be21
kmynyng <kmynyng@gmail.com>
2021-11-03 23:55:08 +0900

    ...

--- front/src/pages/AudioMix/index.tsx
+++ front/src/pages/AudioMix/index.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
-// import { Recorder } from './Recorder'
-
 export function AudioMix() {
   const [devices, setDevices] = useState(null)
 
@@ -123,10 +121,11 @@ function AudioPlayerList(props: AudioPlayerListProps) {
       })}
 
       <p>joinedUser: <br />{joinedIds?.join(' ')}</p><br />
-      recorder <br /><br />
+      {/* recorder <br /><br />
       {
-        joinedIds?.length > 0 && <Recorder recorderRef={recorderRef} ready={true} streamRef={recordedStream} />
-      }
+        joinedIds?.length > 0
+        && <Recorder recorderRef={recorderRef} ready={true} streamRef={recordedStream} />
+      } */}
 
       player
       <audio controls ref={audioRef} autoPlay muted />
@@ -190,90 +189,4 @@ type AudioStreamRefType = {
     stream: MediaStream
     joined: boolean
   }
-}
-
-export type RecorderType = {
-  ready?: boolean
-  streamRef?: React.MutableRefObject<MediaStream>
-  recorderRef?: React.MutableRefObject<MediaRecorder>
-}
-
-export function Recorder(props: RecorderType) {
-  const { recorderRef } = props
-  const chunkRef = useRef(null)
-  const cachingRef = useRef([])
-  const [audioUrl, setAudioUrl] = useState<string>(null)
-  const [recording, setRecording] = useState(false)
-
-  useEffect(() => {
-    recorderRef.current = new MediaRecorder(props.streamRef.current, { mimeType: 'audio/webm' })
-    console.log(props.streamRef.current, recorderRef.current)
-    recorderRef.current.onerror = (err) => {
-      console.log({ err })
-    }
-    recorderRef.current.onstop = () => {
-      console.log('stop')
-    }
-    recorderRef.current.ondataavailable = (e: BlobEvent) => {
-      e.data.arrayBuffer().then((buffer) => {
-        // chunk 5개씩만 보관하여 캐싱
-        if (cachingRef.current?.length >= 5) {
-          cachingRef.current.shift()
-        }
-        cachingRef.current.push(buffer)
-        if (chunkRef.current) {
-          chunkRef.current.push(buffer)
-        }
-
-        console.log(cachingRef.current)
-        console.log(chunkRef.current)
-      })
-    }
-    recorderRef.current.start(1000)
-
-    return () => {
-      if (recorderRef.current.state === 'recording') {
-        recorderRef.current.stop()
-      }
-      props.streamRef.current.getTracks().forEach((track) => track.stop())
-    }
-  }, [])
-
-  const handleClick = () => {
-    if (recording) {
-      setRecording(false)
-      handleStop()
-    } else {
-      setRecording(true)
-      handleStart()
-    }
-  }
-
-  const handleStart = () => {
-    chunkRef.current = [...cachingRef.current]
-  }
-
-  const handleStop = () => {
-    console.log(props.streamRef.current, props.streamRef.current.getTracks())
-    if (recorderRef.current.state === 'recording') {
-      recorderRef.current.stop()
-    }
-    const resultBlob = new Blob(chunkRef.current, { type: 'audio/mp3' })
-    setAudioUrl(URL.createObjectURL(resultBlob))
-    chunkRef.current = null
-  }
-
-  return (
-    <div>
-      <div
-        className='px-4 py-2 m-5 border cursor-pointer border-primary text-primary max-w-max'
-        onClick={handleClick}>
-        {recording ? ' stop ' : 'start'}
-      </div>
-
-      <div>
-        <audio src={audioUrl} controls />
-      </div>
-    </div>
-  )
 }
